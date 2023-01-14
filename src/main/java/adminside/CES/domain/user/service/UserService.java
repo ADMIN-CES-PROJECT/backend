2ab2e7eb702fed5c61307be6ed7969a29f0cddee
kimdonghun <zhfptm12@naver.com>
2023-01-14 21:49:36 +0900

    Feat : 장바구니 목록 확인하는 함수

--- src/main/java/adminside/CES/domain/user/service/UserService.java
+++ src/main/java/adminside/CES/domain/user/service/UserService.java
@@ -1,5 +1,21 @@
 package adminside.CES.domain.user.service;
 
+import adminside.CES.domain.course.entity.Course;
+import adminside.CES.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+//    private final CourseRepository courseRepository;
+
+    public List<Course> getBasket(String userId) {
+        return Collections.emptyList();
+    }
 }
 
