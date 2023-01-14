258c907e680474728700fd4903e7d8d58950476a
kimdonghun <zhfptm12@naver.com>
2023-01-15 01:30:48 +0900

    Feat : 장바구니 담기 기능 구현

--- src/main/java/adminside/CES/domain/user/service/UserService.java
+++ src/main/java/adminside/CES/domain/user/service/UserService.java
@@ -2,22 +2,38 @@ package adminside.CES.domain.user.service;
 
 import adminside.CES.domain.course.entity.Course;
 import adminside.CES.domain.course.repository.CourseRepository;
-import adminside.CES.domain.user.repository.UserRepository;
+import adminside.CES.domain.user.entity.Basket;
+import adminside.CES.domain.user.repository.BasketRepository;
+//import adminside.CES.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
+//    private final UserRepository userRepository;
     private final CourseRepository courseRepository;
-
+    private final BasketRepository basketRepository;
     public List<Course> getBasket(String userId) {
 
         return Collections.emptyList();
+    }
+
+    public void insertBasket(Course course) {
+//        Optional<Course> byId = courseRepository.findById(courseId);
+        Basket basket = new Basket();
+        System.out.println("courseId@@= " + course.getCourseId());
+        basket.setCourseId(course.getCourseId());
+        basket.setStudentId("201802057");
+//        if (byId.isPresent()) {
+//            basketRepository.save(basket);
+//        }
+        basketRepository.save(basket);
+
     }
 }
 
