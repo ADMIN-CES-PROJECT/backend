88034db34bb244e0668f5737eb243d66a80f9152
kimdonghun <zhfptm12@naver.com>
2023-01-15 22:11:29 +0900

    Feat : 장바구니 dto 생성

---
+++ src/main/java/adminside/CES/domain/user/dto/BasketDto.java
@@ -0,0 +1,11 @@
+package adminside.CES.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BasketDto {
+    private String courseId;
+    private String studentId;
+}
