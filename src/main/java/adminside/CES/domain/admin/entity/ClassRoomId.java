e1c40553e83885101a5311618fe720460f651af5
kimdonghun <zhfptm12@naver.com>
2023-01-24 17:17:55 +0900

    Feat : 강의실 테이블의 복합키 인터페이스

---
+++ src/main/java/adminside/CES/domain/admin/entity/ClassRoomId.java
@@ -0,0 +1,24 @@
+package adminside.CES.domain.admin.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class ClassRoomId implements Serializable {
+    @Column(name = "building_bumber")
+    private String buildingNumber;
+
+    @Column(name = "romm_number")
+    private String roomNumber;
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
