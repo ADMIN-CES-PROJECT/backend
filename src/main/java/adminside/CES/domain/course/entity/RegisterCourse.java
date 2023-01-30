f93d2e9520dc1f8e472362c4a6130e3103ba9d9c
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-01-30 13:09:08 +0900

    Feat : 수강신청 연관관계 추가 메소드 구현

--- src/main/java/adminside/CES/domain/course/entity/RegisterCourse.java
+++ src/main/java/adminside/CES/domain/course/entity/RegisterCourse.java
@@ -20,4 +20,16 @@ public class RegisterCourse {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "course_id")
     private Course course;
+
+    public RegisterCourse(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public void setRegisterCourse(Student student, Course course){
+        this.student = student;
+        this.course = course;
+        student.getRegisterCourses().add(this);
+        course.getRegisterCourses().add(this);
+    }
 }
