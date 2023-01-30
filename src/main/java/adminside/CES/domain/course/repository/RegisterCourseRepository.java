90b34bffa4d3fb12a0589c76bbfe1931f5d7c7c2
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-01-30 16:13:49 +0900

    Feat : 수강신청 과목으로 조회

--- src/main/java/adminside/CES/domain/course/repository/RegisterCourseRepository.java
+++ src/main/java/adminside/CES/domain/course/repository/RegisterCourseRepository.java
@@ -1,6 +1,7 @@
 package adminside.CES.domain.course.repository;
 
 import adminside.CES.domain.course.dto.RegisterCourseRequest;
+import adminside.CES.domain.course.entity.Course;
 import adminside.CES.domain.course.entity.RegisterCourse;
 import adminside.CES.domain.user.entity.Student;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,12 @@ public class RegisterCourseRepository {
     public List<RegisterCourse> findAllByStudent(Student student) {
         return em.createQuery("select r from RegisterCourse r where r.student = :student", RegisterCourse.class)
                 .setParameter("student", student)
+                .getResultList();
+    }
+
+    public List<RegisterCourse> findAllByCourse(Course course) {
+        return em.createQuery("select r from RegisterCourse r where r.course = :course", RegisterCourse.class)
+                .setParameter("course", course)
                 .getResultList();
     }
 }
