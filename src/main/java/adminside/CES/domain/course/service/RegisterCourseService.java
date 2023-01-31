6f4ab465728b51000947bc14def4660156d75bbe
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2023-01-31 17:45:15 +0900

    Feat : 수강신청 register 구현

--- src/main/java/adminside/CES/domain/course/service/RegisterCourseService.java
+++ src/main/java/adminside/CES/domain/course/service/RegisterCourseService.java
@@ -1,5 +1,7 @@
 package adminside.CES.domain.course.service;
 
+import adminside.CES.domain.course.entity.Course;
+import adminside.CES.domain.course.entity.RegisterCourse;
 import adminside.CES.domain.course.repository.RegisterCourseRepository;
 import adminside.CES.domain.user.entity.Student;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,25 @@ public class RegisterCourseService {
 
     private final RegisterCourseRepository registerCourseRepository;
 
+    // 미구현
     // private final StudentRepository studentRepository
     // private final CourseRepository courseRepository
 
-    
+    public Long register(int studentId, String courseId) {
+        // 미구현
+        // Student student = studentRepository.findById(studentId);
+        // Course course = ~~~
+        Student student = new Student();
+        student.setId(studentId);
+
+        Course course = new Course();
+        course.setCourseId(courseId);
+
+        RegisterCourse registerCourse = new RegisterCourse();
+        registerCourse.setRegisterCourse(student, course);
+
+        registerCourseRepository.save(registerCourse);
+
+        return registerCourse.getId();
+    }
 }
